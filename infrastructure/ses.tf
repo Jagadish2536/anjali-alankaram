3448274e0301b19410319622fd9e04858650b99d
Anjali Deploy <deploy@anjalialankaram.com>
2026-06-01 11:28:47 +0530

    infra: add SPF and DMARC DNS records to prevent emails landing in spam

--- infrastructure/ses.tf
+++ infrastructure/ses.tf
@@ -29,3 +29,23 @@ resource "aws_route53_record" "ses_dkim" {
   ttl     = "600"
   records = ["${element(aws_ses_domain_dkim.main[0].dkim_tokens, count.index)}.dkim.amazonses.com"]
 }
+
+# -- SPF: Authorize AWS SES to send email on behalf of the domain --
+resource "aws_route53_record" "spf" {
+  count   = var.domain_name != "" ? 1 : 0
+  zone_id = module.alb.route53_zone_id
+  name    = var.domain_name
+  type    = "TXT"
+  ttl     = "600"
+  records = ["v=spf1 include:amazonses.com ~all"]
+}
+
+# -- DMARC: Policy for email authentication failures --
+resource "aws_route53_record" "dmarc" {
+  count   = var.domain_name != "" ? 1 : 0
+  zone_id = module.alb.route53_zone_id
+  name    = "_dmarc.${var.domain_name}"
+  type    = "TXT"
+  ttl     = "600"
+  records = ["v=DMARC1; p=none; rua=mailto:jagadishvarma99@gmail.com; ruf=mailto:jagadishvarma99@gmail.com; fo=1"]
+}
