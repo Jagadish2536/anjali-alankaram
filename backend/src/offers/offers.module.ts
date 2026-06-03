184521f9cacb916a90fe1143c8f613222fbc8dc3
Anjali Deploy <deploy@anjalialankaram.com>
2026-06-03 23:02:10 +0530

    fix: import PrismaModule in OffersModule to resolve database dependency

--- backend/src/offers/offers.module.ts
+++ backend/src/offers/offers.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OffersService } from './offers.service';
 import { OffersController } from './offers.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [OffersController],
   providers: [OffersService],
   exports: [OffersService],
